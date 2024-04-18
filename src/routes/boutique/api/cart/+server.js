0db2c7fe0cf747045878cd77801152a58929eea8
Theo <theobaillargeon@hotmail.com>
2024-04-18 15:51:55 -0400

    ff

--- src/routes/boutique/api/cart/+server.js
+++ src/routes/boutique/api/cart/+server.js
@@ -1,7 +1,7 @@
 import { Cart } from '$lib/server/models/Cart';
 //d d
 import { json } from '@sveltejs/kit';
-
+// 666
 export async function POST({ cookies }) {
 	try {
 		const cart = new Cart();
