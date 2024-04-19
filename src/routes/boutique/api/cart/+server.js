43ca52763671e1d20fe3626f584dbd2fd4f82312
Marc <baillargeonmarc@gmail.com>
2024-04-19 10:18:24 -0400

    mon changement

--- src/routes/boutique/api/cart/+server.js
+++ src/routes/boutique/api/cart/+server.js
@@ -1,12 +1,7 @@
 import { Cart } from '$lib/server/models/Cart';
 //d d
 import { json } from '@sveltejs/kit';
-<<<<<<< HEAD
-// message de Marc
-
-=======
 // 666
->>>>>>> 0db2c7fe0cf747045878cd77801152a58929eea8
 export async function POST({ cookies }) {
 	try {
 		const cart = new Cart();
